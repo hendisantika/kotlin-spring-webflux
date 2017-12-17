8613046bca3c0a4eee154d041ed8960c33444f18
Hendi Santika <hendisantika@gmail.com>
2017-12-17 22:36:17 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/kotlinspringwebflux/KotlinSpringWebfluxApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.kotlinspringwebflux
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KotlinSpringWebfluxApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinSpringWebfluxApplication::class.java, *args)
+}
