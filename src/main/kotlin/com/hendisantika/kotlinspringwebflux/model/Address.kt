aa9763119cb29b58e29958025da1a47542340c9f
Hendi Santika <hendisantika@gmail.com>
2017-12-18 03:59:44 +0700

    #3 - Add Address Entity

---
+++ src/main/kotlin/com/hendisantika/kotlinspringwebflux/model/Address.kt
@@ -0,0 +1,16 @@
+package com.hendisantika.kotlinspringwebflux.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-spring-webflux
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/12/17
+ * Time: 03.57
+ * To change this template use File | Settings | File Templates.
+ */
+data class Address(
+        var street: String? = null,
+        var postcode: String? = null
+)
